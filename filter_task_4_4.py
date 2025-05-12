271830792aa46129da695148064b190539fbd303
jonibek99 <jonibekannaev5@gmail.com>
2025-05-13 00:06:48 +0500

    filter4

--- filter_task_4_4.py
+++ filter_task_4_4.py
@@ -6,4 +6,6 @@ def filter_task_4_4(names):
     Returns:
         list of names that start with "A"
     """
-    return
+    return list(filter(lambda x: x[0]=='A',names))
+a=["Alice", "Bob", "Anna", "Charlie"]
+print(filter_task_4_4(a))
