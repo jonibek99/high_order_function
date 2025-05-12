9a01998f22b7b1fdae5caa84e8c4b3e5b773d91a
jonibek99 <jonibekannaev5@gmail.com>
2025-05-13 00:14:14 +0500

    max5

--- max_task_2_5.py
+++ max_task_2_5.py
@@ -6,4 +6,7 @@ def max_task_2_5(people):
     Returns:
         dictionary of person with highest age
     """
-    return
+    a=list(map(lambda i: i['age'],people))
+    return max(a)
+v=[{"name": "Alice", "age": 30}, {"name": "Bob", "age": 45}, {"name": "Tom", "age": 28}]
+print(max_task_2_5(v))
