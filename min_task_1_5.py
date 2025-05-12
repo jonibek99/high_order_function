a793140689fbbac37668014db6dbab40d603e19f
jonibek99 <jonibekannaev5@gmail.com>
2025-05-13 00:17:02 +0500

    min5

--- min_task_1_5.py
+++ min_task_1_5.py
@@ -6,4 +6,6 @@ def min_task_1_5(tuples):
     Returns:
         tuple with smallest second element
     """
-    return
+    return min(list(map(lambda i: i[1],tuples)))
+a=[(1, 3), (2, 2), (3, 1)]
+print(min_task_1_5(a))
