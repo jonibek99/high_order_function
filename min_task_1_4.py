d1c8ec05f89a5f2dba01ffa47fd9b889dd4083fd
jonibek99 <jonibekannaev5@gmail.com>
2025-05-13 00:16:40 +0500

    min4

--- min_task_1_4.py
+++ min_task_1_4.py
@@ -6,4 +6,6 @@ def min_task_1_4(dictionaries):
     Returns:
         dictionary with smallest value
     """
-    return
+    return min(list(map(lambda i: i['a'],dictionaries)))
+a= [{'a': 5}, {'a': 3}, {'a': 7}]
+print(min_task_1_4((a)))
